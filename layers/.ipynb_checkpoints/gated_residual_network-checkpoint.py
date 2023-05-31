521bfa930718a3f9015bf63d4dc072b9edebd090
emremidilli <70489609+emremidilli@users.noreply.github.com>
2023-06-01 01:42:28 +0300

    fine-tuning model ongoing

--- layers/.ipynb_checkpoints/gated_residual_network-checkpoint.py
+++ layers/.ipynb_checkpoints/gated_residual_network-checkpoint.py
@@ -3,47 +3,50 @@ from gated_linear_unit import gated_linear_unit
 class gated_residual_network(tf.keras.layers.Layer):
     '''
       Args:
-          iInputSize (int): Size of the input
-          iHiddenSize (int): Size of the hidden layer
-          iOutputSize (int): Size of the output layer
+          iModelDims (int): Size of the hidden layer. Normally, input, hidden and output sizes are different. 
+                            However, our paper already plans to input transformed inputs from pre-trained encoders.
+                            That's why all inputs will have dimension of model_dims.
           fDropout (float): Fraction between 0 and 1 corresponding to the degree of fDropout used
-          iContextSize (int): Size of the static context vector
+          bIsWithStaticCovariate (boolean): Size of the static context vector. 
     '''
-    def __init__(self, iInputSize, iHiddenSize, iOutputSize, fDropout, iContextSize=None):
+    def __init__(self, iModelDims, fDropout, bIsWithStaticCovariate=False):
         super().__init__()
 
-        self.iInputSize = iInputSize
-        self.iOutputSize = iOutputSize
-        self.iContextSize = iContextSize
-        self.iHiddenSize = iHiddenSize
+        self.bIsWithStaticCovariate = bIsWithStaticCovariate
+        self.iModelDims = iModelDims
         self.fDropout = fDropout
-
-        if self.iInputSize != self.iOutputSize:
-            self.oSkipConnectionDense = tf.keras.layers.Dense(units = self.iOutputSize, activation = None)
-
+        
         # Context vector c
-        if self.iContextSize != None:
-            self.oDense_c = tf.keras.layers.Dense(units = self.iHiddenSize, activation = None) 
+        if self.bIsWithStaticCovariate == True:
+            self.oDense_c = tf.keras.layers.Dense(units = self.iModelDims, activation = None) 
 
         # Dense & ELU
-        self.oDense1 = tf.keras.layers.Dense(units = self.iHiddenSize, activation = None)
+        self.oDense1 = tf.keras.layers.Dense(units = self.iModelDims, activation = None)
         self.oElu = tf.keras.layers.ELU()
 
         # Dense & Dropout
-        self.oDense2 = tf.keras.layers.Dense(self.iHiddenSize,  self.iOutputSize)
+        self.oDense2 = tf.keras.layers.Dense(self.iModelDims)
         self.oDropout = tf.keras.layers.Dropout(self.fDropout)
 
         # Gate, Add & Norm
-        self.oGate = gated_linear_unit(self.iOutputSize)
+        self.oGate = gated_linear_unit(self.iModelDims)
         self.oLayerNorm = tf.keras.layers.LayerNormalization()
 
         
-    def call(self, x, c=None):
         
-        if self.iInputSize!=self.iOutputSize:
-            a = self.oSkipConnectionDense(x)
-        else:
-            a = x
+        
+
+    '''
+        Args:
+            x: should a transformation of a time patch in following format (None, 1, model_dims)
+               output of an encoder representation (e.g. DisERT) will be in format of (None, number_of_patches, model_dims)
+               for a time step, there will be for 4 representations coming from DisERT, TicERT, TreERT and SeaERT.
+               in TFT paper, model_dims is mentioned as "transformed inputs" in Figure 2.
+            c: is optional input. In case static covariates will be an input, it should be accepted.
+    '''
+    def call(self, x, c=None):
+
+        a = x
         
         x = self.oDense1(x)
 
--- layers/variable_selection_network.py
+++ layers/variable_selection_network.py
@@ -1,16 +1,68 @@
 import tensorflow as tf
-from gated_linear_unit import gated_linear_unit
+from gated_residual_network import gated_residual_network
+
 class variable_selection_network(tf.keras.layers.Layer):
     '''
       Args:
-          
+          iModelDims: common model_dims for all input representations
+          iNrOfKnownVars: array since there may be multiple known features
+          iNrOfObservedVars: array since there may be multiple observed features
+          iNrOfChannels: number of channels. For DisERT, TicERT, TreERT, SeaERT there should be seperate representation.
+          fDropout: dropout rate fro GRUs.
     '''
-    def __init__(self, ):
+    def __init__(self, 
+                 iModelDims,
+                 iNrOfKnownVars,
+                 iNrOfObservedVars,
+                 iNrOfChannels,
+                 fDropout
+                ):
         super().__init__()
+        
+        self.iModelDims = iModelDims
+        self.fDropout = fDropout
+        
+        
+        self.aGrus = []
+        iTotalVars = (4 + iNrOfKnownVars + iNrOfObservedVars) * iNrOfChannels
+        for i in range(iTotalVars):
+            self.aGrus.append(
+                gated_residual_network(iModelDims, fDropout, bIsWithStaticCovariate=False)
+            )
+            # weights of GRUs are not shared accross each variable.
+            # but they are shared accross each time patch.
+            
 
+        self.oFlatten = tf.keras.layers.Flatten()
+        self.oGruFlatten = gated_residual_network(iTotalVars * iModelDims, fDropout, bIsWithStaticCovariate=True)
+        self.oSoftmax = tf.keras.layers.Softmax(dim=-1)
         
-    def call(self, x, c=None):
+    '''
+        Args:
+            x: (None, 1, model_dims, nr_of_variables)
+               combination of all encoder representations for a given time patch.
+               nr_of_variables = (4 + number_of_knowns + number_of_observeds) x nr_of_channels
+               in TFT paper, 
+                   - nr_of_variables: is mentioned as dimension of \mu_{\chi}
+                   - model_dims: is mentioned as transformed inputs (or embedding)
+            c_s: (None, 1, model_dims x nr_of_channels)
+               encoded static covariate vector.
+    '''
+    def call(self, x, c_s):
+        a = self.oFlatten(x)
+        v = self.oGruFlatten(a, c_s)
         
-
+        v = self.oSoftmax(v)
         
-        return x
+        arr = []
+        i = 0
+        for oGru in self.aGrus:
+            arr.append(oGru(x[:,:,i]))
+            i = i + 1
+            
+        b = tf.stack(arr)    
+        
+        c = b * v
+        c = tf.reduce_sum(c, -1)
+        
+        return c
