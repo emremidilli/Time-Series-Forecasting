feb0efbf1c437026eda6a4a8f8759e13863fc38e
emremidilli <70489609+emremidilli@users.noreply.github.com>
2023-06-14 03:43:34 +0300

    updates

--- models/bootstrap_aggregation.py
+++ models/bootstrap_aggregation.py
@@ -14,28 +14,31 @@ class bootstrap_aggregation(tf.keras.Model):
     '''
     Args:
         aEncoderRepresentations (array): array where each element is instance of general_pre_training_model.
+        b_with_decoder (boolean): default True.
     '''
         
-    def __init__(self, aEncoderRepresentations, **kwargs):
+    def __init__(self, aEncoderRepresentations, b_with_decoder = True ,**kwargs):
         super().__init__(**kwargs)
         
         self.iNrOfChannels = 3
         self.iNrOfQuantiles = 3
-
+        
         self.oLoss = BinaryCrossentropy()
         self.oMetric = AUC(name = 'AUC')
         
         self.aEncoderRepresentations = aEncoderRepresentations
+        self.b_with_decoder = b_with_decoder
         
         self.oDenseAggregater = tf.keras.layers.Dense(units = 1, use_bias = False, activation = None) # bias is not used for aggregated model not to have impact of bias.
         
-        self.oSppDecoder = Spp_Decoder(
-            iFfnUnits = self.iNrOfQuantiles * 3 # due to quantiles. 3 means one-hot categories of signs {positive, negative and zero}
-        )        
+        if self.b_with_decoder == True:
+            self.oSppDecoder = Spp_Decoder(
+                iFfnUnits = self.iNrOfQuantiles * 3 # due to quantiles. 3 means one-hot categories of signs {positive, negative and zero}
+            )        
 
-        self.oRppDecoder = Rpp_Decoder(
-            iFfnUnits = self.iNrOfQuantiles * (self.iNrOfChannels + 1) # +1 is because 0 rank is assigned for the positions of special tokens.
-        )
+            self.oRppDecoder = Rpp_Decoder(
+                iFfnUnits = self.iNrOfQuantiles * (self.iNrOfChannels + 1) # +1 is because 0 rank is assigned for the positions of special tokens.
+            )
         
         
     '''
@@ -55,17 +58,39 @@ class bootstrap_aggregation(tf.keras.Model):
             x_bootstrapped.append(oEncoderRepresentation(x))
             
         # (None, nr_of_positions, model_dims, nr_of_models)
-        x_bootstrapped = tf.stack(x_bootstrapped, axis =  3) 
+        if len(self.aEncoderRepresentations) > 1:
+            x_bootstrapped = tf.stack(x_bootstrapped, axis =  3) 
         
-        x_aggregated = self.oDenseAggregater(x_bootstrapped)
+            x_aggregated = self.oDenseAggregater(x_bootstrapped)
+
+            x_aggregated = tf.squeeze(x_aggregated, 3)
+        else:
+            x_aggregated = x_bootstrapped
+            
+            
         
-        x_aggregated = tf.squeeze(x_aggregated, 3)
-        
-        y_spp = self.oSppDecoder(x_aggregated)
-        
-        y_rpp = self.oRppDecoder(x_aggregated)
-        
-        return [y_spp, y_rpp]
+        if self.b_with_decoder == True:
+            y_spp = self.oSppDecoder(x_aggregated)
+
+            y_rpp = self.oRppDecoder(x_aggregated)
+
+            return [y_spp, y_rpp]
+        else:
+            return x_aggregated
     
+    
+    
+    def TransferLearning(self, oModelFrom):
+        iIndexDecoder = 0
+        for s in oModelFrom.weights: 
+            if ('__decoder' in s.name) or ('bootstrap_aggregation' not in s.name): #every layer until decoder is already common between models.
+                break
+            else:
+                iIndexDecoder = iIndexDecoder + 1
+        
+        
+        aNewWeights = self.get_weights()
+        aNewWeights[:iIndexDecoder] = oModelFrom.get_weights()[:iIndexDecoder]
+        self.set_weights(aNewWeights)
 
         
--- training/.ipynb_checkpoints/aggregate-checkpoint.py
+++ training/.ipynb_checkpoints/aggregate-checkpoint.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append( '../')
 
-from training.constants import TEST_SIZE, NR_OF_EPOCHS, MINI_BATCH_SIZE
+from training.constants import TEST_SIZE, NR_OF_EPOCHS, MINI_BATCH_SIZE, BATCH_SIZE
 
 from hyperparameter_tuning.general_pre_training import oGetArchitectureTuner
 
@@ -26,7 +26,6 @@ from tensorflow.keras.metrics import AUC
 import shutil
 
 
-
 def aGetRepresentationModels(sRepresentationName):
     aModels = []
     for root, dirs, files in os.walk(ARTIFACTS_FOLDER):
@@ -46,99 +45,108 @@ def aGetRepresentationModels(sRepresentationName):
                     aModels.append( oModel)
                     
                     
-                    if len(aModels):
-                        return aModels
-                    
+                   
     return aModels
                     
 
 if __name__ == '__main__':
     
-    sDatasetName = 'dist'
-    sRepresentationName = f'{sDatasetName.title()[:3]}ERT'
-    sModelArtifactPath = f'{ARTIFACTS_FOLDER}\\Aggregation\\{sRepresentationName}\\'
-    if os.path.exists(sModelArtifactPath) == True:
-        shutil.rmtree(sModelArtifactPath)
+    
+    for sDatasetName in ['dist', 'tic', 'tre', 'known', 'observed', 'sea']:
+        sRepresentationName = f'{sDatasetName.title()[:3]}ERT'
+        sModelArtifactPath = f'{ARTIFACTS_FOLDER}\\Aggregation\\{sRepresentationName}\\'
+        if os.path.exists(sModelArtifactPath) == True:
+            shutil.rmtree(sModelArtifactPath)
 
-    os.makedirs(sModelArtifactPath)           
-    
-    
-    X_spp = np.load(f'{SIGN_OF_PATCH_PREDICTION_DATA_FOLDER}\\X_{sDatasetName}.npy')    
-    Y_spp = np.load(f'{SIGN_OF_PATCH_PREDICTION_DATA_FOLDER}\\Y_{sDatasetName}.npy')
-    
-    Y_rpp = np.load(f'{RANK_OF_PATCH_PREDICTION_DATA_FOLDER}\\Y_{sDatasetName}.npy')
+        os.makedirs(sModelArtifactPath)           
 
 
-    dataset = Dataset.from_tensor_slices((X_spp, Y_spp, Y_rpp))
-    train_dataset, validation_dataset = split_dataset(
-        dataset,
-        right_size = TEST_SIZE,
-        shuffle = False
-    )
-    
-    
-    train_dataset = train_dataset.batch(len(train_dataset)).get_single_element()
-    validation_dataset = validation_dataset.batch(len(validation_dataset)).get_single_element()
-    
-    
-    X_spp_train, Y_spp_train, Y_rpp_train = train_dataset
-    X_spp_val, Y_spp_val, Y_rpp_val = validation_dataset
-    
-    
-    aRepresentationModels = aGetRepresentationModels(sRepresentationName)
-    
-    # set representation model layers not trainable
-    for oModel in aRepresentationModels:
-        for oLayer in oModel.layers:
-            oLayer.trainable = False
+        X_spp = np.load(f'{SIGN_OF_PATCH_PREDICTION_DATA_FOLDER}\\X_{sDatasetName}.npy')    
+        Y_spp = np.load(f'{SIGN_OF_PATCH_PREDICTION_DATA_FOLDER}\\Y_{sDatasetName}.npy')
 
-    
-    oAggregationModel = bootstrap_aggregation(aRepresentationModels)
-    
-    oCsvLogger = CSVLogger(f'{sModelArtifactPath}logs.log', separator=";", append=False)
-    
+        Y_rpp = np.load(f'{RANK_OF_PATCH_PREDICTION_DATA_FOLDER}\\Y_{sDatasetName}.npy')
 
 
-    oReduceLr = ReduceLROnPlateau(
-        monitor='loss', 
-        factor=0.2, 
-        patience= 3, 
-        min_lr=0.0001
-    )
-
-    oEarlyStopping = EarlyStopping(
-        monitor='val_loss', 
-        patience=5, 
-        restore_best_weights=True
-    )
-
-
-    oAggregationModel.compile(
-        loss = BinaryCrossentropy(), 
-        metrics = AUC(name = 'AUC'),
-        optimizer= Adam(
-            learning_rate=ExponentialDecay(
-                initial_learning_rate=1e-3,
-                decay_steps=100000,
-                decay_rate=0.96
-            ),
-            beta_1 = 0.90
+        dataset = Dataset.from_tensor_slices((X_spp, Y_spp, Y_rpp))
+        train_dataset, validation_dataset = split_dataset(
+            dataset,
+            right_size = TEST_SIZE,
+            shuffle = False
         )
-    )
-    
-    
-    oAggregationModel.fit(
-        X_spp_train, 
-        [Y_spp_train, Y_rpp_train], 
-        epochs= NR_OF_EPOCHS, 
-        batch_size=1024, 
-        verbose=1,
-        validation_data = (X_spp_val, [Y_spp_val, Y_rpp_val]),
-        validation_batch_size = 1024,
-        callbacks = [oCsvLogger, oReduceLr, oEarlyStopping]
-    )
 
-    oAggregationModel.save_weights(
-        sModelArtifactPath,
-        save_format ='tf'
-    )
+        train_dataset = train_dataset.shuffle(len(train_dataset)).take(BATCH_SIZE)
+        train_dataset = train_dataset.batch(len(train_dataset)).get_single_element()
+        validation_dataset = validation_dataset.batch(len(validation_dataset)).get_single_element()
+
+
+        X_spp_train, Y_spp_train, Y_rpp_train = train_dataset
+        X_spp_val, Y_spp_val, Y_rpp_val = validation_dataset
+
+
+        aRepresentationModels = aGetRepresentationModels(sRepresentationName)
+
+        # set representation model layers not trainable
+        for oModel in aRepresentationModels:
+            for oLayer in oModel.layers:
+                oLayer.trainable = False
+
+
+        oAggregationModel = bootstrap_aggregation(aRepresentationModels)
+
+        oCsvLogger = CSVLogger(f'{sModelArtifactPath}logs.log', separator=";", append=False)
+
+
+
+        oReduceLr = ReduceLROnPlateau(
+            monitor='loss', 
+            factor=0.2, 
+            patience= 3, 
+            min_lr=0.0001
+        )
+
+        oEarlyStopping = EarlyStopping(
+            monitor='val_loss', 
+            patience=10, 
+            restore_best_weights=True
+        )
+
+
+        oAggregationModel.compile(
+            loss = BinaryCrossentropy(), 
+            metrics = AUC(name = 'AUC'),
+            optimizer= Adam(
+                learning_rate=ExponentialDecay(
+                    initial_learning_rate=1e-3,
+                    decay_steps=100000,
+                    decay_rate=0.96
+                ),
+                beta_1 = 0.90
+            )
+        )
+
+
+        oAggregationModel.fit(
+            X_spp_train, 
+            [Y_spp_train, Y_rpp_train], 
+            epochs= NR_OF_EPOCHS, 
+            batch_size= MINI_BATCH_SIZE, 
+            verbose=1,
+            validation_data = (X_spp_val, [Y_spp_val, Y_rpp_val]),
+            validation_batch_size = MINI_BATCH_SIZE,
+            callbacks = [oCsvLogger, oReduceLr, oEarlyStopping]
+        )
+
+        oAggregationModel.save_weights(
+            sModelArtifactPath,
+            save_format ='tf'
+        )
+
+
+        oFinalModel = bootstrap_aggregation(aRepresentationModels, b_with_decoder = False)
+        oFinalModel.predict(X_spp_train) # needed for just transfoer learning.
+        oFinalModel.TransferLearning(oAggregationModel)
+        oFinalModel.save( 
+            filepath = f'{sModelArtifactPath}\\final_model\\', 
+            overwrite=True, 
+            save_format='tf'
+        )
