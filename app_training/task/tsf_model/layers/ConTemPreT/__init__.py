6711190809ad0361923019ada11b1b99e4a83768
emremidilli <yunusemremidilli@gmail.com>
2023-08-23 17:23:37 +0300

    updates

--- app_training/task/tsf_model/layers/ConTemPreT/__init__.py
+++ app_training/task/tsf_model/layers/ConTemPreT/__init__.py
@@ -1,5 +1,4 @@
 from .decoder import MppDecoder, ProjectionHead  # noqa: F401
 from .embedding import PositionEmbedding, Time2Vec  # noqa: F401
 from .transformer_encoder import TransformerEncoder  # noqa: F401
-from .variable_selection import VariableSelection  # noqa: F401
 from .representation import Representation  # noqa: F401
